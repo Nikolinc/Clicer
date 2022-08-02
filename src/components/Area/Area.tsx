619e9c5407dd2d396294cf304087c85f1d7ff221
Nikolinc <tsigulskynikita@gmail.com>
2022-08-02 16:07:24 +0500

    add header

--- src/components/Area/Area.tsx
+++ src/components/Area/Area.tsx
@@ -1,7 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import { news } from "../../Data/news";
 import "./styleArea.scss";
 
 export default function Area() {
-  return <div className="Area" id="objact"></div>;
+  return (
+    <div className="Area" id="objact">
+      <div className="header">
+        <div className="leftMenu">
+          <button className="setting">setting</button>
+          <button className="statistic">statistic</button>
+        </div>
+        <div className="news">{news[getRandomInt(4)]}</div>
+        <div className="rightMenu">
+          <button className="information">information</button>
+          <button className="experience">experience</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
 }
