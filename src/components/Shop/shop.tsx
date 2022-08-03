335581a2fddfaf726956fa193a15ccc79960926b
Nikolinc <tsigulskynikita@gmail.com>
2022-08-03 10:30:41 +0500

    add store Section

--- src/components/Shop/shop.tsx
+++ src/components/Shop/shop.tsx
@@ -2,5 +2,10 @@ import React from "react";
 import "./StyleShop.scss";
 
 export default function Shop() {
-  return <div className="shop" id = "VerticalBorder"></div>;
+  return (
+    <div className="shop" id="VerticalBorder">
+      <div className="titleShop">Store</div>
+      <div id="HorizontalBorder" className="storeSection upgradeBox"></div>
+    </div>
+  );
 }
