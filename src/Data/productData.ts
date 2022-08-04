1f13b76bede1a10f7096686596ca73d08e0601d6
Nikolinc <tsigulskynikita@gmail.com>
2022-08-04 16:54:20 +0500

    add product data

---
+++ src/Data/productData.ts
@@ -0,0 +1,11 @@
+import ProductType from "../Types/product";
+
+export const CursorData:ProductType = {
+  name: "Cursor",
+  Price: 15,
+  description: "auto click every 10 seconds",
+  imagePositionX: "0px",
+  imagePositionY: "0px",
+  CookiesInSecond: 100,
+  increase: 0.2
+}
