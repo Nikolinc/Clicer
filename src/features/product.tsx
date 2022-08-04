83f6a891f4f476c22b82be54166bdb2eff8f6db9
Nikolinc <tsigulskynikita@gmail.com>
2022-08-04 16:52:41 +0500

    git add parent class product

---
+++ src/features/product.tsx
@@ -0,0 +1,42 @@
+import ProductType from "../Types/product";
+
+export default class Product implements ProductType {
+  name: string;
+  increase: number;
+  Price: number;
+  description: string;
+  imagePositionX: string;
+  imagePositionY: string;
+  CookiesInSecond: number;
+  count = 0;
+
+  constructor(product: ProductType) {
+    this.name = product.name;
+    this.Price = product.Price;
+    this.description = product.description;
+    this.imagePositionX = product.imagePositionX;
+    this.imagePositionY = product.imagePositionY;
+    this.CookiesInSecond = product.CookiesInSecond;
+    this.increase = product.increase;
+  }
+
+
+  buyProduct() {
+    this.count++;
+    this.CookiesInSecond +=this.CookiesInSecond;
+    this.Price = this.Price * (this.increase * this.count);
+    return this.count;
+  }
+
+  getPrice() {
+    return this.Price;
+  }
+
+  getAll(): ProductType {
+    return this;
+  }
+  
+  getPosition(){
+    return [this.imagePositionX ,this.imagePositionY] 
+  }
+}
