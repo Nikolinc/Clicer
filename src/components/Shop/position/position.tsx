1b815e4938d3ef70e6089a469d323e38979f918a
Nikolinc <tsigulskynikita@gmail.com>
2022-08-04 10:35:48 +0500

    git upgrade style and fix:output image

--- src/components/Shop/position/position.tsx
+++ src/components/Shop/position/position.tsx
@@ -1,7 +1,22 @@
+import { util } from "prettier";
 import React from "react";
+import "./StylePosition.scss";
+import { position } from "../../../Data/news";
 
-export default function Position(){
-  return(
-    <div className="position"></div>
-  )
+export default function Position() {
+  console.log(position);
+  return position.map((pos) => (
+    <>
+      <div className={`position ${pos.id.toString()}`} key={pos.id}>
+        <div
+          className={`  image ${pos.id.toString()}`}
+          style={{
+            backgroundPositionX: `${pos.x}` + "px",
+            backgroundPositionY: `${pos.y}` + "px",
+          }}
+          key={pos.id}
+        ></div>
+      </div>
+    </>
+  ));
 }
--- src/components/Shop/shop.tsx
+++ src/components/Shop/shop.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Position from "./position/position";
 import "./StyleShop.scss";
 
 export default function Shop() {
   return (
     <div className="shop" id="VerticalBorder">
       <div className="titleShop">Store</div>
-      <div id="HorizontalBorder" className="storeSection upgradeBox"></div>
+      <div id="HorizontalBorder" className="storeSection upgradeBox">
+        <Position />
+      </div>
     </div>
   );
 }
