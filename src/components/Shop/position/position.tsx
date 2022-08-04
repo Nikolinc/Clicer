3ec3a2b65075bf4776d1e36fb3c5592ccfb60c3a
Nikolinc <tsigulskynikita@gmail.com>
2022-08-04 12:14:13 +0500

    fix:Type 'Element[]' is missing the following properties from type 'Element':

--- src/components/Shop/position/position.tsx
+++ src/components/Shop/position/position.tsx
@@ -1,22 +1,23 @@
 import { util } from "prettier";
-import React from "react";
+import React, { JSXElementConstructor } from "react";
 import "./StylePosition.scss";
 import { position } from "../../../Data/news";
 
 export default function Position() {
-  console.log(position);
-  return position.map((pos) => (
+  return (
     <>
-      <div className={`position ${pos.id.toString()}`} key={pos.id}>
-        <div
-          className={`  image ${pos.id.toString()}`}
-          style={{
-            backgroundPositionX: `${pos.x}` + "px",
-            backgroundPositionY: `${pos.y}` + "px",
-          }}
-          key={pos.id}
-        ></div>
-      </div>
+      {position.map((pos) => (
+        <div className={`position ${pos.id.toString()}`} key={pos.id}>
+          <div
+            className={`  image ${pos.id.toString()}`}
+            style={{
+              backgroundPositionX: `${pos.x}` + "px",
+              backgroundPositionY: `${pos.y}` + "px",
+            }}
+            key={pos.id}
+          ></div>
+        </div>
+      ))}
     </>
-  ));
+  );
 }
