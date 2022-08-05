cb00241886c90f2cd6175b747ee52b1dae428a4a
Nikolinc <tsigulskynikita@gmail.com>
2022-08-05 15:50:02 +0500

    deploing prod

--- src/Types/product.tsx
+++
@@ -1,9 +0,0 @@
-export default interface ProductType {
-  name: string;
-  Price: number;
-  description: string;
-  imagePositionX: number;
-  imagePositionY: number;
-  CookiesInSecond: number;
-  increase: number;
-}
