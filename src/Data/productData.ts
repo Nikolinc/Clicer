086d8e16f6935abcbd87973431885b91a7456955
Nikolinc <tsigulskynikita@gmail.com>
2022-08-05 12:49:26 +0500

    update product data

--- src/Data/productData.ts
+++ src/Data/productData.ts
@@ -4,8 +4,8 @@ export const CursorData:ProductType = {
   name: "Cursor",
   Price: 15,
   description: "auto click every 10 seconds",
-  imagePositionX: "0px",
-  imagePositionY: "0px",
+  imagePositionX: 0,
+  imagePositionY: 0,
   CookiesInSecond: 100,
   increase: 0.2
 }
